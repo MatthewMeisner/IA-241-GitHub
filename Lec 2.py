ead12256f97f4a20189219e481a8dc0899424b7d
EC2 Default User <ec2-user@ip-172-31-94-205.ec2.internal>
2022-01-26 21:39:07 +0000

    lecture_2

---
+++ Lec 2.py
@@ -0,0 +1,47 @@
+"""
+this is a regional comment
+"""
+
+#print(hello world)
+
+#print(type (123) )
+
+#print( type (123.) )
+
+#print(type ("123") )
+
+#print("hello world")
+
+#print('hello world')
+
+#print("Tom's cat")
+
+#print('hello world'.upper())
+
+#print('Hello World'.lower())
+
+#print('   Hello World  '.strip())
+
+# print('hello world'.split())
+# print('hello, world'.split(","))
+# print('hello world'.split("o"))
+
+# print(2**3)
+
+# my_int = 123
+# print(my_int)
+
+# my_int = 321
+# print(my_int)
+
+# my_int = 123
+
+# print(my_int)
+
+# my_float = 123.
+
+# print(my_float/2)
+
+my_str = hello_world
+
+print(my_str.upper("_"))
